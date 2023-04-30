32d03143d718c8ed1fe69a1a8b2be80e4d69ccf2
staadegh <33016148+staadegh@users.noreply.github.com>
2023-04-30 15:47:20 -0600

    Created files styles.css, index.html, script.js
    Created files styles.css, index.html, script.js for Full Stack Immersion exercise 1.3 task

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let pokemonList =[{name: 'Charmander', height: 0.5, types:['Leave', 'Tree'], Abilities:"Blaze", EVs:"2 Defence"},
+                  {name: 'Metapod', height: 0.6, types:['Green', 'Sour'], Abilities: "Run-away", EVs: "1 Speed"},
+                  {name: 'Weedle', height: 1.7, types:['Tall', 'Short'], Abilities:"Shed-skin", EVs:"2 Speed"},
+                  {name: 'Horsey', height: 0.5, types:['Big', 'Finest'], Abilities:"Shed-skin", EVs:"3 Speed" },
+                  {name: 'Fearow', height: 0.4, types:['Thin', 'Earliest'], Abilities:"Sniper", EVs:"2 Speed" }];
+
+// Part 1 of exercise 1.3.
+for (let i = 0; i <pokemonList.length; i++) { 
+    document.write(pokemonList[i].name)
+    document.write("<br>") // Creates a carriage return; next line
+}
+ 
+document.write("<br>")
+
+for (let i = 0; i <pokemonList.length; i++) {
+    document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" ) 
+    document.write("<br>") // Creates a carriage return; next line
+}
+
+// Part 3 of exercise 1.3.
+document.write("<br>")
+
+for (let i = 0; i <pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.0) // Checks if the current pokemon's height is greater than 1.0; a threshold of 1.0 is chosen for the height.
+       document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")- Wow, that's big!" ) 
+    else 
+       document.writeln(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" )
+    document.write("<br>") // Creates a carriage return; next line 
+}
+
