b07ba90e5f2d66f75b3a97d0f2aaad80417a1728
staadegh <33016148+staadegh@users.noreply.github.com>
2023-05-01 22:11:41 -0600

    Updated files index.html and js\script.js for exercise 1.5 task
    Updated files index.html and js\script.js for exercise 1.5 task

--- js/script.js
+++ js/script.js
@@ -1,27 +1,89 @@
-let pokemonList =[{name: 'Charmander', height: 0.5, types:['Leave', 'Tree'], Abilities:"Blaze", EVs:"2 Defence"},
-                  {name: 'Metapod', height: 0.6, types:['Green', 'Sour'], Abilities: "Run-away", EVs: "1 Speed"},
-                  {name: 'Weedle', height: 1.7, types:['Tall', 'Short'], Abilities:"Shed-skin", EVs:"2 Speed"},
-                  {name: 'Horsey', height: 0.5, types:['Big', 'Finest'], Abilities:"Shed-skin", EVs:"3 Speed" },
-                  {name: 'Fearow', height: 0.4, types:['Thin', 'Earliest'], Abilities:"Sniper", EVs:"2 Speed" }];
+let pokemonRepository = function() {
+  let pokemonList = [
+    {
+      name: "Charmander",
+      height: 0.5,
+      types: ["Leave", "Tree"],
+      Abilities: "Blaze",
+      EVs: "2 Defence",
+    },
+    {
+      name: "Metapod",
+      height: 0.6,
+      types: ["Green", "Sour"],
+      Abilities: "Run-away",
+      EVs: "1 Speed",
+    },
+    {
+      name: "Weedle",
+      height: 1.7,
+      types: ["Tall", "Short"],
+      Abilities: "Shed-skin",
+      EVs: "2 Speed",
+    },
+    {
+      name: "Horsey",
+      height: 0.5,
+      types: ["Big", "Finest"],
+      Abilities: "Shed-skin",
+      EVs: "3 Speed",
+    },
+    {
+      name: "Fearow",
+      height: 0.4,
+      types: ["Thin", "Earliest"],
+      Abilities: "Sniper",
+      EVs: "2 Speed",
+    },
+  ];
 
-// Part 1 of exercise 1.3.
-for (let i = 0; i <pokemonList.length; i++) { 
-    document.write(pokemonList[i].name+ "<br>")
-}
- 
-document.write("<br>")
+  function getAll() {
+    return pokemonList;
+  }
 
-for (let i = 0; i <pokemonList.length; i++) {
-    document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "<br>") 
-}
+  function add(pokemon) {
+    if (typeof pokemon === "object")
+      // Bonus Task solution; Ensures that only objects can be added.
+      pokemonList.push(pokemon);
+  }
 
-// Part 3 of exercise 1.3.
-document.write("<br>")
+  function findPokemon(searchName) {
+    // Bonus Task solution; use filter function to find Pokemon.
+    let pokemonFound = pokemonList.filter((pkle) => pkle.name === searchName);
 
-for (let i = 0; i <pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.0) // Checks if the current pokemon's height is greater than 1.0; a threshold of 1.0 is chosen for the height.
-       document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")- Wow, that's big!" + "<br>" ) 
-    else 
-       document.writeln(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "<br>" )
-}
+    if (pokemonFound.length) console.log(`Pokemon ${searchName} Is Found!`);
+    else console.log(`Pokemon ${searchName} Is Not Found!`);
+  }
+  return {
+    getAll: getAll,
+    add: add,
+    findPokemon: findPokemon, // Bonus Task solution; use filter function to find Pokemon.
+  };
+}();
 
+// Use pokemonRepository.getAll() is used in the codes below to get the pokemonList from the above IIFE.
+pokemonRepository
+  .getAll()
+  .forEach((pkle) => document.write(pkle.name + "<br>"));
+
+document.write("<br>");
+
+pokemonRepository
+  .getAll()
+  .forEach((pkle) =>
+    document.write(pkle.name + "(height: " + pkle.height + ")" + "<br>")
+  );
+
+document.write("<br>");
+
+pokemonRepository.getAll().forEach((pkle) => {
+  if (pkle.height > 1.0)
+    // Checks if the current pokemon's height is greater than 1.0; a threshold of 1.0 is chosen for the height.
+    document.write(
+      pkle.name + "(height: " + pkle.height + ")- Wow, that's big!" + "<br>"
+    );
+  else document.writeln(pkle.name + "(height: " + pkle.height + ")" + "<br>");
+});
+
+pokemonRepository.findPokemon("Metapod2"); // Bonus Task solution; use filter function to find Pokemon Metapod2. Result: Pokemon Metapod2 Is Not Found!
+pokemonRepository.findPokemon("Metapod"); // Bonus Task solution; use filter function to find Pokemon Metapod. Result: Pokemon Metapod Is Found!
