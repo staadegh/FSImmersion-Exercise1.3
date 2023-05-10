ce4ca0f23a86669cdedeb59c38e6a1c5b0734e7a
staadegh <33016148+staadegh@users.noreply.github.com>
2023-05-10 13:16:40 -0600

    Created files index.html, css\styles.css, js\scripts for exercise 1.10
    exercise 1.10 - Created files index.html, css\styles.css, js\scripts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=15";
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=14";
 
   function getAll() {
     return pokemonList;
